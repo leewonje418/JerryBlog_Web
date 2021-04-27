c76d4de8e9b66d3892e4d89986bfcab6f1476d59
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-27 20:55:52 +0900

    [UPDATE] reducer create 함수 재작

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import { History } from 'history';
+
+const createRootReducer = (history: History<any>) => 
+    combineReducers({
+        router: connectRouter(history),
+    });
+
+export default createRootReducer;
