f96c9a45f0e26ed7734a7eadeab374294484a48b
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-29 22:12:24 +0900

    [DELETE]

--- src/types/redux.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
-  }
-  
-  const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
