73aab3ebf2bbd67f531c45471680f7c8a317b7f0
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-10 22:08:03 +0900

    [UPDATE] loginmodal 재작

--- src/components/auth/LoginModal.tsx
+++ src/components/auth/LoginModal.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {} from 'reactstrap';
 
-const LoginModal = () => {};
+const LoginModal = () => {
+    const [modal, setModal] = useState(false);
+    const [localMsg, setLocalMsg] = useState('');
+    const [form, setValues] = useState({
+        email: '',
+        password: ''
+    })
+    const dispatch = useDispatch();
+    const { errorMsg } = useSelector((state: any) => state.auth);
+};
 
 export default LoginModal;
