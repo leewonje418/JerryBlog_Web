423e551fec199cf39e66b1251cde51996a51af9b
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-10 22:08:44 +0900

    [UPDATE] authReducer 불러오기

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
+import authReducer from './authReducer';
 
 const createRootReducer = (history: History<any>) => 
     combineReducers({
         router: connectRouter(history),
+        auth: authReducer
     });
 
 export default createRootReducer;
